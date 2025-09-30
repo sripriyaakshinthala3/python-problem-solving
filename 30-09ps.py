cce7d290c03db1b7de517b66d9c60807c41572b4
sripriya <sripriyaakshinthala3@gmail.com>
2025-09-30 11:34:07 +0530

    bubble sort practice assg

---
+++ 30-09ps.py
@@ -0,0 +1,23 @@
+# list1 = [1,2,101,3,-3,55,-322]#[1,2,3,-3,55,-322,101]
+#to get max element at last index in list we here use swap
+# for j in range(0,len(list1)-1):
+#     for i in range(len(list1)-1-j):
+#         if list1[i] > list1[i+1]:
+#             list1[i],list1[i+1]=list1[i+1],list1[i]
+# print(list1)        
+
+# list1 = [1,2,101,3,-3,55,-322]
+# for i in range(0,len(list1)-1):
+#     for j in range(len(list1)-1):
+#         if list1[j] < list1[j+1]:
+#             list1[j],list1[j+1]=list1[j+1],list1[j]
+# print(list1)            
+
+# list1 = ['1','2','101','3','-3','55','-322']
+# for i in range(0,len(list1)-1):
+#     for j in range(len(list1)-1):
+#         if list1[j] < list1[j+1]:
+#             list1[j],list1[j+1]=list1[j+1],list1[j]
+# print(list1)
+#['55', '3', '2', '101', '1', '-322', '-3']    
+# we will get dictionary like order will be more clear if we take string with alphabets or words with alpha    
